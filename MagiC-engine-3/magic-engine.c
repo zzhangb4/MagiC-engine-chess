979c0d70081f870b0a89d0e8e40976817a76ab4c
zzhangb4 <65728949+zzhangb4@users.noreply.github.com>
2020-12-10 19:46:47 -0500

    Update magic-engine.c

--- MagiC-engine-3/magic-engine.c
+++ MagiC-engine-3/magic-engine.c
@@ -7,7 +7,7 @@
 
 
 #define WAC1 "r1b1k2r/ppppnppp/2n2q2/2b5/3NP3/2P1B3/PP3PPP/RN1QKB1R w KQkq - 0 1"
-#define PERFT "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
+#define PERFT "q4rk1/2p3pp/1b4n1/pP1p1pP1/3pn3/2P2P1P/1P2Q2K/RNB1R3 w - - 0 23"
 
 int main(int argc, char *argv[]) {
 
